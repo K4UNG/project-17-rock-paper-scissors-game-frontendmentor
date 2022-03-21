1e421f9592d504f347dff155b4a97c282b524bfc
K4UNG <kgzinhein.my@gmail.com>
2022-03-21 20:27:15 +0630

    first finished version

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const rules = document.querySelector('.rules');
+const mode = document.querySelector('.switch');
 const modal = document.querySelector('.modal');
 const closeBtn = document.getElementById('close');
+const list = document.querySelector('.mode');
 
-const triangle = document.querySelector('.shape');
-const triangleBtns = triangle.querySelectorAll('button');
+let triangle = document.querySelector('.shape');
+let triangleBtns = triangle.querySelectorAll('button');
 const triangleScore = document.querySelector('.score');
 const reset = document.getElementById('reset');
 
@@ -18,20 +20,30 @@ triangleScore.textContent = getScore();
 
 const properties = [
     {
-        img: "images/icon-paper.svg",
-        color: "var(--paper-gradient)",
-        side: "var(--paper-side-result)"
-    },
-    {
         img: "images/icon-scissors.svg",
         color: "var(--scissors-gradient)",
         side: "var(--scissors-side-result)"
     },
     {
+        img: "images/icon-paper.svg",
+        color: "var(--paper-gradient)",
+        side: "var(--paper-side-result)"
+    },
+    {
         img: "images/icon-rock.svg",
         color: "var(--rock-gradient)",
         side: "var(--rock-side-result)"
-    }
+    },
+    {
+        img: "images/icon-lizard.svg",
+        color: "var(--lizard-gradient)",
+        side: "var(--lizard-side-result)"
+    },
+    {
+        img: "images/icon-spock.svg",
+        color: "var(--spock-gradient)",
+        side: "var(--spock-side-result)"
+    },
 ];
 
 reset.onclick = () => {
@@ -61,25 +73,41 @@ closeBtn.onclick = () => {
     }
 }
 
-triangleBtns.forEach(btn => {
-    btn.onclick = () => {
-        you = btn.dataset.rank;
-        house = Math.floor(Math.random() * 3);
-        displayResult(parseInt(you), parseInt(house));
-    }
-});
+// triangleBtns.forEach(btn => {
+//     btn.onclick = () => {
+//         you = btn.dataset.rank;
+//         house = Math.floor(Math.random() * 3);
+//         displayResult(parseInt(you), parseInt(house));
+//     }
+// });
 
 function displayResult(player, ai) {
+    mode.style.display = 'none';
     let result;
     let score = parseInt(getScore());
-    if ((player + 1) % 3 === ai) {
-        result = 'you lose';
-        score--;
-    } else if (player === ai) {
-        result = 'draw';
+    if (triangle.classList.contains('five')) {
+
+        if ((player + 1) % 5 === ai || (player + 3) % 5 === ai) {
+            result = 'you win';
+            score++;
+        } else if (player === ai) {
+            result = 'draw';
+        } else {
+            result = 'you lose';
+            score--;
+        }
     } else {
-        result = 'you win';
-        score++;
+
+        if ((player + 2) % 3 === ai) {
+            result = 'you lose';
+            score--;
+        } else if (player === ai) {
+            result = 'draw';
+        } else {
+            result = 'you win';
+            score++;
+        }
+
     }
     setScore(score);
     triangle.style.display = 'none';
@@ -105,6 +133,7 @@ function displayResult(player, ai) {
         you.closest('.result').style.transform = 'unset';
         you.parentElement.classList.remove('win');
         house.parentElement.classList.remove('win');
+        mode.style.display = 'block';
     }
 
     setTimeout(() => {
@@ -147,4 +176,125 @@ window.onresize = () => {
             result.style.transform = 'unset';
         });
     }
+}
+
+mode.onclick = () => {
+    triangle.classList.toggle('five');
+    changeMode();
+
+}
+
+changeMode();
+
+function changeMode() {
+    if (triangle.classList.contains('five')) {
+
+
+        modal.querySelector('.wrapper>img').setAttribute('src', 'images/image-rules-bonus.svg');
+        list.innerHTML = `<li>
+        <p>rock</p>
+      </li>
+      <li>
+        <p>paper</p>
+      </li>
+      <li>
+        <p>scissors</p>
+      </li>
+      <li>
+        <p>lizard</p>
+      </li>
+      <li>
+        <p>spock</p>
+      </li>`;
+
+        triangle.innerHTML = `
+        <img src="images/bg-pentagon.svg" alt="pentagon background">
+  
+        <div class="scissors-container-b container">
+          <button aria-label="scissors button" data-rank="0">
+            <div class="inner">
+              <img src="images/icon-scissors.svg" alt="paper">
+            </div>
+          </button>
+        </div>
+        <div class="paper-container-b container">
+          <button aria-label="paper button" data-rank="1">
+            <div class="inner">
+              <img src="images/icon-paper.svg" alt="paper">
+            </div>
+          </button>
+        </div>
+        <div class="rock-container-b container">
+          <button aria-label="rock button" data-rank="2">
+            <div class="inner">
+              <img src="images/icon-rock.svg" alt="paper">
+            </div>
+          </button>
+        </div>
+        <div class="lizard-container-b container">
+          <button aria-label="lizard button" data-rank="3">
+            <div class="inner">
+              <img src="images/icon-lizard.svg" alt="scissors">
+            </div>
+          </button>
+        </div>
+        <div class="spock-container-b container">
+          <button aria-label="spock button" data-rank="4">
+            <div class="inner">
+              <img src="images/icon-spock.svg" alt="rock">
+            </div>
+          </button>
+        </div>`;
+        list.querySelectorAll('p').forEach(p => {
+            p.style.fontSize = '1.5rem';
+        })
+
+    } else {
+
+        modal.querySelector('.wrapper>img').setAttribute('src', 'images/image-rules.svg');
+        list.innerHTML = `<li>
+        <p>rock</p>
+      </li>
+      <li>
+        <p>paper</p>
+      </li>
+      <li>
+        <p>scissors</p>
+      </li>`
+        triangle.innerHTML = `
+        <img src="images/bg-triangle.svg" alt="triangle background">
+  
+        <div class="paper-container container">
+          <button aria-label="paper button" data-rank="1">
+            <div class="inner">
+              <img src="images/icon-paper.svg" alt="paper">
+            </div>
+          </button>
+        </div>
+        <div class="scissors-container container">
+          <button aria-label="scissors button" data-rank="0">
+            <div class="inner">
+              <img src="images/icon-scissors.svg" alt="scissors">
+            </div>
+          </button>
+        </div>
+        <div class="rock-container container">
+          <button aria-label="rock button" data-rank="2">
+            <div class="inner">
+              <img src="images/icon-rock.svg" alt="rock">
+            </div>
+          </button>
+        </div>`;
+
+    }
+    triangle = document.querySelector('.shape');
+    triangleBtns = triangle.querySelectorAll('button');
+
+    triangleBtns.forEach(btn => {
+        btn.onclick = () => {
+            you = btn.dataset.rank;
+            house = Math.floor(Math.random() * 3);
+            displayResult(parseInt(you), parseInt(house));
+        }
+    });
 }
